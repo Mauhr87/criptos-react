906fe7e0d267f2e237b0a4b91abe0f26647f2892
Mauricio Hidalgo Ríos <mauriciohidalgorios@Mauricios-MacBook-Pro.local>
2022-08-03 11:43:30 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+import ImagenCripto from './assets/img/imagen-criptos.png'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  width: 90%;
+  margin: 0 auto;
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    display: block;
+    background-color: #66a2fe;
+    margin: 10px auto 0 auto;
+  }
+`
+
+function App() {
+
+  return (
+    <Contenedor>
+      <Imagen
+        src={ImagenCripto}
+        alt='imagenes criptomonedas'
+        />
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+      </div>
+    </Contenedor>
+  )
+}
+
+export default App
